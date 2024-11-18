4760bedb29fa9d3c3ffb87c4067762d596c55256
WisenextTime <141509640+WisenextTime@users.noreply.github.com>
2024-11-18 16:08:29 +0800

    Fixed 'TouchJoy.gd'

--- Scripts/GodotScripts/TouchJoy.gd
+++ Scripts/GodotScripts/TouchJoy.gd
@@ -9,7 +9,7 @@ var index = -1
 
 func _ready() -> void:
 	if OS.get_name() == "Android":
-		show()
+		get_parent().show()
 
 func _on_button_pressed() -> void:
 	if !pressed:
@@ -24,7 +24,7 @@ func _on_button_released() -> void:
 	$Center.global_position = startPos
 
 func _input(event: InputEvent) -> void:
-	if pressed and event is InputEventScreenTouch:
+	if pressed and (event is InputEventScreenTouch or event is InputEventScreenDrag):
 		if index==-1:
 			index = event.index
 			global_position = event.position - Vector2(maxLength,maxLength)
@@ -33,19 +33,19 @@ func _input(event: InputEvent) -> void:
 			if(getVector.length()>maxLength):
 				getVector = getVector.normalized() * maxLength
 			$Center.global_position = global_position + getVector
-			if getVector.x>maxLength/2:
+			if getVector.x>maxLength*diedZone:
 				Input.action_press("MoveRight")
 				await get_tree().create_timer(1/Engine.get_frames_per_second()).timeout
 				Input.action_release("MoveRight")
-			if getVector.x<maxLength/2:
+			if getVector.x<-maxLength*diedZone:
 				Input.action_press("MoveLeft")
 				await get_tree().create_timer(1/Engine.get_frames_per_second()).timeout
 				Input.action_release("MoveLeft")
-			if getVector.y>maxLength/2:
+			if getVector.y>maxLength*diedZone:
 				Input.action_press("MoveDown")
 				await get_tree().create_timer(1/Engine.get_frames_per_second()).timeout
 				Input.action_release("MoveDown")
-			if getVector.y<maxLength/2:
+			if getVector.y<-maxLength*diedZone:
 				Input.action_press("MoveUp")
 				await get_tree().create_timer(1/Engine.get_frames_per_second()).timeout
 				Input.action_release("MoveUp")
